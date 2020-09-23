257adbebcb37c8b23d18bec89545ebdee2d0ff2d
maisjamil1 <maisjamil17118@gmail.com>
2020-09-24 00:19:22 +0300

    1

---
+++ users/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+# Create your tests here.
+# User Acceptance Tests
+# Verify the creation of a new user with email and password
+# Verify that duplicate emails are not allowed
